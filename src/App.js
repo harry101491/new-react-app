b6f747e54453063c48bbbe40ae2efaa4a31acd15
Harshit Pareek <hp1014@nyu.edu>
2018-08-21 09:51:34 -0700

    feat(adding Person component)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: "Harshit",
+        age: 26
+      },
+      {
+        name: "Aniket",
+        age: 29,
+        hobbies: "Cricket"
+      },
+      {
+        name: "Ricardo",
+        age: 26
+      }
+    ]
+  };
+
+  switchNameHandler = () => {
+    console.log("Inside the Switch Name Handler");
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          I am a React App!
+        </h1>
+        <button
+          onClick={ this.switchNameHandler }
+        >
+          Switch Name
+        </button>
+        <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age } />
+        <Person name={ this.state.persons[1].name } age={ this.state.persons[1].age } >
+          { this.state.persons[1].hobbies }
+        </Person>
+        <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
       </div>
     );
   }
